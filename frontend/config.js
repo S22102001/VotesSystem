8b6ee603b9d0f60e0d0916bfd63c18f0374b715b
S22102001 <Shahar.girtler@gmail.com>
2026-01-26 18:33:59 +0200

    feat: config.js

---
+++ frontend/config.js
@@ -0,0 +1,3 @@
+//  update lambda Invoke URL
+export const VOTE_API_BASE = "https://oazi6zq4u3.execute-api.eu-west-1.amazonaws.com";
+export const RESULTS_API_BASE = "https://hjxg67w5mc.execute-api.eu-west-1.amazonaws.com";
